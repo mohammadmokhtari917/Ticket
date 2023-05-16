5d155a0b5d04df67c5581346bcaa642dfea724d8
Mohammad Mokhtari <118061522+mohammadmokhtari917@users.noreply.github.com>
2023-05-16 11:55:30 -0800

    Creating admin view panel

---
+++ views/site/dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var yii\web\View $this */
+use app\models\Details;
+$details=Details::findOne(1);
+$this->title = $details->name;
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="">تیکت</h1>
+
+        <p class="lead">مشکلات و خواسته های خود را باما در میان  بگذارید</p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+
+    </div>
+</div>
